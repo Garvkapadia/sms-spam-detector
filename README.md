8588ca884a666ac9c8f673c97bf41ae2be94ab04
Garv Kapadia <kapadiagarv88@gmail.com>
2026-06-24 01:11:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# SMS Spam Detection System
+
+A Machine Learning web application that predicts whether an SMS message is Spam or Not Spam.
+
+## Features
+- SMS Spam Classification
+- NLP Text Preprocessing
+- TF-IDF Vectorization
+- Streamlit Frontend
+- Real-time Prediction
+
+## Tech Stack
+- Python
+- Scikit-learn
+- NLTK
+- Pandas
+- Streamlit
+
+## Project Workflow
+1. Data Cleaning
+2. Text Preprocessing
+3. TF-IDF Vectorization
+4. Model Training
+5. Spam Prediction
+6. Streamlit Deployment
+
+## Run Locally
+
+```bash
+pip install -r requirements.txt
+streamlit run app.py
+```
+
+## Author
+Garv Kapadia
---
+++ app.py
@@ -0,0 +1,47 @@
+
+import streamlit as st
+import pickle
+
+
+
+tfidf=pickle.load(open('vectorizer.pkl','rb'))
+model=pickle.load(open('model.pkl','rb'))
+import string
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import nltk
+ps = PorterStemmer()
+stop_words = set(stopwords.words('english'))
+def transform_text(message):
+    message = message.lower()
+    message = nltk.word_tokenize(message)
+
+    y = []
+    for i in message:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    message = y[:]
+    y.clear()
+
+    for i in message:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+st.title('SMS Spam Detection System')
+input_sms=st.text_input("Enter the message:")
+if st.button('Predict'):
+ transformed_sms=transform_text(input_sms)
+ input_vector=tfidf.transform([transformed_sms])
+ result=model.predict(input_vector)[0]
+ if result==0:
+    st.header('NOT SPAM')
+ else:
+    st.header('SPAM')
